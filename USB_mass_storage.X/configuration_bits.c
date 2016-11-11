0adef0dab52a6fb1861fc0181b4af57cc2536822
Douglas Katz <djk289@cornell.edu>
2016-11-11 13:50:06 -0500

    Created project file and added config bits

---
+++ USB_mass_storage.X/configuration_bits.c
@@ -0,0 +1,46 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#ifdef __XC32
+    #include <xc.h>          /* Defines special funciton registers, CP0 regs  */
+#endif
+
+#include <plib.h>            /* Include to use PIC32 peripheral libraries     */
+
+/******************************************************************************/
+/* Configuration Bits                                                         */
+/*                                                                            */
+/* Refer to 'C32 Configuration Settings' under the Help > Contents            */
+/* > C32 Toolchain in MPLAB X IDE for available C32 PIC32 Configurations. For */
+/* additional information about what the hardware configurations mean in      */
+/* terms of device operation, refer to the device datasheet 'Special Features'*/
+/* chapter. XC32 documentation is available under Help > Contents >           */
+/* XC32 C Compiler.                                                           */
+/*                                                                            */
+/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
+/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
+/* Configuration Bits, a user controllable configuration bits window is       */
+/* available to Generate Configuration Bits source code which the user can    */
+/* paste into this project.                                                   */
+/******************************************************************************/
+
+#pragma config UPLLEN   = ON            // USB PLL Enabled
+#pragma config FPLLMUL  = MUL_20        // PLL Multiplier
+#pragma config UPLLIDIV = DIV_2         // USB PLL Input Divider
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider
+#pragma config FPLLODIV = DIV_2         // PLL Output Divider
+#pragma config FPBDIV   = DIV_1         // Peripheral Clock divisor
+#pragma config FWDTEN   = OFF           // Watchdog Timer 
+#pragma config WDTPS    = PS1           // Watchdog Timer Postscale
+#pragma config FCKSM    = CSDCMD        // Clock Switching & Fail Safe Clock Monitor
+#pragma config OSCIOFNC = OFF           // CLKO Enable
+#pragma config POSCMOD  = HS           // Primary Oscillator HS = high speed crystal (If using Primary OSC turn to HS)
+#pragma config IESO     = OFF           // Internal/External Switch-over
+#pragma config FSOSCEN  = OFF           // Secondary Oscillator Enable
+#pragma config FNOSC    = PRIPLL        // Oscillator Selection
+#pragma config CP       = OFF           // Code Protect
+#pragma config BWP      = OFF           // Boot Flash Write Protect
+#pragma config PWP      = OFF           // Program Flash Write Protect
+#pragma config ICESEL   = ICS_PGx1      // ICE/ICD Comm Channel Select
+#pragma config DEBUG    = OFF           // Debugger Disabled for Starter Kit
---
+++ USB_mass_storage.X/interrupts.c
@@ -0,0 +1,147 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#ifdef __XC32
+    #include <xc.h>          /* Defines special funciton registers, CP0 regs  */
+#endif
+
+#include <plib.h>            /* Include to use PIC32 peripheral libraries     */
+#include <sys/attribs.h>     /* For __ISR definition                          */
+#include <stdint.h>          /* For uint32_t definition                       */
+#include <stdbool.h>         /* For true/false definition                     */
+
+/******************************************************************************/
+/* Interrupt Vector Options                                                   */
+/******************************************************************************/
+/*                                                                            */
+/* VECTOR NAMES:                                                              */
+/*                                                                            */
+/* _CORE_TIMER_VECTOR          _COMPARATOR_2_VECTOR                           */
+/* _CORE_SOFTWARE_0_VECTOR     _UART_2A_VECTOR                                */
+/* _CORE_SOFTWARE_1_VECTOR     _I2C_2A_VECTOR                                 */
+/* _EXTERNAL_0_VECTOR          _SPI_2_VECTOR                                  */
+/* _TIMER_1_VECTOR             _SPI_2A_VECTOR                                 */
+/* _INPUT_CAPTURE_1_VECTOR     _I2C_4_VECTOR                                  */
+/* _OUTPUT_COMPARE_1_VECTOR    _UART_3_VECTOR                                 */
+/* _EXTERNAL_1_VECTOR          _UART_2_VECTOR                                 */
+/* _TIMER_2_VECTOR             _SPI_3A_VECTOR                                 */
+/* _INPUT_CAPTURE_2_VECTOR     _I2C_3A_VECTOR                                 */
+/* _OUTPUT_COMPARE_2_VECTOR    _UART_3A_VECTOR                                */
+/* _EXTERNAL_2_VECTOR          _SPI_4_VECTOR                                  */
+/* _TIMER_3_VECTOR             _I2C_5_VECTOR                                  */
+/* _INPUT_CAPTURE_3_VECTOR     _I2C_2_VECTOR                                  */
+/* _OUTPUT_COMPARE_3_VECTOR    _FAIL_SAFE_MONITOR_VECTOR                      */
+/* _EXTERNAL_3_VECTOR          _RTCC_VECTOR                                   */
+/* _TIMER_4_VECTOR             _DMA_0_VECTOR                                  */
+/* _INPUT_CAPTURE_4_VECTOR     _DMA_1_VECTOR                                  */
+/* _OUTPUT_COMPARE_4_VECTOR    _DMA_2_VECTOR                                  */
+/* _EXTERNAL_4_VECTOR          _DMA_3_VECTOR                                  */
+/* _TIMER_5_VECTOR             _DMA_4_VECTOR                                  */
+/* _INPUT_CAPTURE_5_VECTOR     _DMA_5_VECTOR                                  */
+/* _OUTPUT_COMPARE_5_VECTOR    _DMA_6_VECTOR                                  */
+/* _SPI_1_VECTOR               _DMA_7_VECTOR                                  */
+/* _I2C_3_VECTOR               _FCE_VECTOR                                    */
+/* _UART_1A_VECTOR             _USB_1_VECTOR                                  */
+/* _UART_1_VECTOR              _CAN_1_VECTOR                                  */
+/* _SPI_1A_VECTOR              _CAN_2_VECTOR                                  */
+/* _I2C_1A_VECTOR              _ETH_VECTOR                                    */
+/* _SPI_3_VECTOR               _UART_4_VECTOR                                 */
+/* _I2C_1_VECTOR               _UART_1B_VECTOR                                */
+/* _CHANGE_NOTICE_VECTOR       _UART_6_VECTOR                                 */
+/* _ADC_VECTOR                 _UART_2B_VECTOR                                */
+/* _PMP_VECTOR                 _UART_5_VECTOR                                 */
+/* _COMPARATOR_1_VECTOR        _UART_3B_VECTOR                                */
+/*                                                                            */
+/* Refer to the device specific .h file in the C32 Compiler                   */
+/* pic32mx\include\proc directory for a complete Vector and IRQ mnemonic      */
+/* listings for the PIC32 device.                                             */
+/*                                                                            */
+/* PRIORITY OPTIONS:                                                          */
+/*                                                                            */
+/* (default) IPL0AUTO, IPL1, IPL2, ... IPL7 (highest)                         */
+/*                                                                            */
+/* Example Shorthand Syntax                                                   */
+/*                                                                            */
+/* void __ISR(<Vector Name>,<PRIORITY>) user_interrupt_routine_name(void)     */
+/* {                                                                          */
+/*     <Clear Interrupt Flag>                                                 */
+/* }                                                                          */
+/*                                                                            */
+/* For more interrupt macro examples refer to the C compiler User Guide in    */
+/* the C compiler /doc directory.                                             */
+/*                                                                            */
+/******************************************************************************/
+/* Interrupt Routines                                                         */
+/******************************************************************************/
+
+/* TODO Add interrupt routine code here. */
+
+/****************************************************************************
+  Function:
+    void __ISR(_CHANGE_NOTICE_VECTOR, ipl2) _ChangeNoticeHandler(void)
+
+  Description:
+    Change Notice (CN) ISR.  This routine is called when a change occures on 
+    one of the configured CN ports.
+
+  Precondition:
+    None
+
+  Parameters:
+    None
+
+  Return Values:
+    None
+
+  Remarks:
+    None
+  ***************************************************************************/
+void __ISR(_CHANGE_NOTICE_VECTOR, ipl4) _ChangeNoticeHandler(void)
+{
+    unsigned int temp;
+
+    // clear the mismatch condition
+    temp = mPORTDRead();
+
+    // clear the interrupt flag
+    mCNClearIntFlag();
+
+    // Set the flag to indicate the switch has been pressed.
+    commandEntrySwitchPressed = TRUE;
+
+}
+
+
+/****************************************************************************
+  Function:
+    void __attribute__((__interrupt__, auto_psv)) _T3Interrupt(void)
+
+  Description:
+    Timer ISR, used to update the tick count.  Since we are using Timer 3, we
+    can also trigger an A/D conversion off of the timer.
+
+  Precondition:
+    None
+
+  Parameters:
+    None
+
+  Return Values:
+    None
+
+  Remarks:
+    None
+  ***************************************************************************/
+
+void __ISR(_TIMER_3_VECTOR, ipl2) _T3Interrupt(void)
+{
+    if (IFS0 & PIC32MX_TIMER3_INTERRUPT)
+    {
+        // Clear the interrupt flag
+        mT3ClearIntFlag();
+
+        // Count the timer tick
+        currentTick++;
+    }
+}
