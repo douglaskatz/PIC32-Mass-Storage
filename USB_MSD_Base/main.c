af24069b622a0bdcc77001ec0a1d58f09fb57dcf
Lab User <djk289@cornell.edu>
2016-12-13 18:22:00 -0500

    Added Base Code Project

--- USB_MSD_Base/main.c
+++ USB_MSD_Base/main.c
@@ -7,10 +7,9 @@
 
 //------------------------------------------------------------------------------
 /* 
- * File_Write_Demo
- * This program writes a string to a file. The program will write to a file 
- * specified by a file name. If a file by that name does not exist the file will
- * be created and then written to.
+ * USB_MSD_Base
+ * This project contains the minimum code needed to run USB MSD programs and is
+ * meant to be built on top of.
  */
 //------------------------------------------------------------------------------
 
@@ -37,7 +36,7 @@
 #pragma config WDTPS    = PS1           // Watchdog Timer Postscale
 #pragma config FCKSM    = CSDCMD        // Clock Switching & Fail Safe Clock Monitor
 #pragma config OSCIOFNC = OFF           // CLKO Enable
-#pragma config POSCMOD  = HS           // Primary Oscillator HS = high speed crystal (If using Primary OSC turn to HS)
+#pragma config POSCMOD  = HS            // Primary Oscillator HS = high speed crystal (If using Primary OSC turn to HS)
 #pragma config IESO     = OFF           // Internal/External Switch-over
 #pragma config FSOSCEN  = OFF           // Secondary Oscillator Enable
 #pragma config FNOSC    = PRIPLL        // Oscillator Selection
@@ -47,99 +46,17 @@
 #pragma config ICESEL   = ICS_PGx1      // ICE/ICD Comm Channel Select
 #pragma config DEBUG    = OFF           // Debugger Disabled for Starter Kit
 
-FSFILE * myFile;
-volatile BOOL deviceAttached;
-
-/* LED Definitions */
-#define _TRIS_LEDGREEN TRISAbits.TRISA0
-#define _LEDGREEN LATAbits.LATA0
-
-#define _TRIS_LEDRED TRISBbits.TRISB4
-#define _LEDRED LATBbits.LATB4
-
-#define _TRIS_LEDYELLOW TRISAbits.TRISA4
-#define _LEDYELLOW LATAbits.LATA4
-
 int main(void)
-{
-    // Set up LEDs
-    _TRIS_LEDGREEN = 0;
-    _TRIS_LEDRED = 0;
-    _TRIS_LEDYELLOW = 0;
-    _LEDGREEN = 0;
-    _LEDRED = 0;
-    _LEDYELLOW = 0;
-    
-    // Enable the cache for the best performance
-    CheKseg0CacheOn();
-
+{ 
     INTEnableSystemMultiVectoredInt();
-    
-    //UART STUFF
-    // specify PPS group, signal, logical pin name
-    PPSInput (2, U2RX, RPB8); //Assign U2RX to pin RPB11 -- Physical pin 22 on 28 PDIP
-    PPSOutput(4, RPB14, U2TX); //Assign U2TX to pin RPB10 -- Physical pin 21 on 28 PDIP
-    
-   // init the uart2
-    UARTConfigure(UART2, UART_ENABLE_PINS_TX_RX_ONLY);
-    UARTSetLineControl(UART2, UART_DATA_SIZE_8_BITS | UART_PARITY_NONE | UART_STOP_BITS_1);
-    UARTSetDataRate(UART2, PBCLK, 9600);
-    UARTEnable(UART2, UART_ENABLE_FLAGS(UART_PERIPHERAL | UART_RX | UART_TX));
-
-    deviceAttached = FALSE;
-
     //Initialize the stack
     USBInitialize(0);
-    _LEDYELLOW = 1;
-    printf("Standby...\n\r");
     while(1)
     {
         //USB stack process function (Call to advance USB state machine)
         USBTasks();
         
-        _LEDRED = 0;
-        _LEDGREEN = 0;
-        
-        //if thumbdrive is plugged in
-        if(USBHostMSDSCSIMediaDetect())
-        {
-            deviceAttached = TRUE;
-            _LEDRED = 1;
-            _LEDGREEN = 0;
 
-            //now a device is attached
-            //See if the device is attached and in the right format
-            if(FSInit())
-            {
-                printf("Writing...\n\r");
-                // Open file or create file if none exists in write mode ("w")
-                // NOTE: File names must be less than eight characters before the radix
-                // NOTE: Extension must also be less than three characters
-                myFile = FSfopen("write.txt","w");
-                
-                size_t stringLength = strlen("ECE 4750");
-                
-                // Write the string to the file "myFile"
-                FSfwrite("ECE 4750", 1, stringLength, myFile );
-                
-                // Signal when the write has finished
-                printf("Finished Writing");
-                printf("\n\r");
-                // Always make sure to close the file so that the data gets
-                // written to the drive.
-                FSfclose(myFile);
-
-                //Just sit here until the device is removed.
-                while(deviceAttached == TRUE)
-                {
-                    USBTasks();
-                    _LEDRED = 0;
-                    _LEDGREEN = 1;
-                }
-            }
-        }
-        _LEDRED = 0;
-        _LEDGREEN = 0;
     }
     return 0;
 }
@@ -166,7 +83,7 @@ int main(void)
 
   Parameters:
     BYTE address    - Address of device where event occurred
-    USB_EVENT event - Identifies the event that occured
+    USB_EVENT event - Identifies the event that occurred
     void *data      - Pointer to event-specific data
     DWORD size      - Size of the event-specific data
 
@@ -196,7 +113,6 @@ BOOL USB_ApplicationEventHandler( BYTE address, USB_EVENT event, void *data, DWO
             // The PIC24F with the Explorer 16 cannot turn off Vbus through software.
 
             //This means that the device was removed
-            deviceAttached = FALSE;
             return TRUE;
 
         case EVENT_HUB_ATTACH:
---
+++ USB_MSD_Base/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=USB_MSD_Base.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/USB_MSD_Base.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=usbmsdbase.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/usbmsdbase.tar
